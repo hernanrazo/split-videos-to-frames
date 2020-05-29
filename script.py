ada21d5834b917d81db8555755d83f9f540eb4f1
Hernan Razo <hernanrazo@gmail.com>
2020-05-29 18:31:14 -0500

    removed unnecessary .destroyAllWindows() call

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ import os
 cap = cv2.VideoCapture('YOUR_FILE_PATH_HERE')
 path_to_save = 'YOUR_FILE_PATH_HERE'
 
-current_frame = 0
+current_frame = 1
 
 while(True):
 
@@ -16,7 +16,6 @@ while(True):
 
     # Save frame as a jpg file
     name = 'frame' + str(current_frame) + '.jpg'
-    
     print ('Creating: ' + name)
     cv2.imwrite(os.path.join(path_to_save, name), frame)
 
@@ -27,9 +26,6 @@ while(True):
     if not ret:
         break
 
-
 #release capture 
 cap.release()
-cv2.destroyAllWindows()
-
 print('done')
